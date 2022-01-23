f28e57580ec375e517261d8ab5a7cb16619359c0
liuyfab <liuyongfeng83@gmail.com>
2022-01-22 22:21:05 -0600

    add thought controller file

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,17 +1,38 @@
-const { Thought } = require('../../models');
 //Setting up the routes for express Server . 
 const router = require("express").Router();
+const {
+    getAllThoughts,
+    addThought,
+    getThoughtById,
+    updateThought,
+    removeThought,
+    addReaction,
+    removeReaction
+  } = require('../../controllers/thought-controller');
+  
+// Get all thoughts
+router
+  .route('/')
+  .get(getAllThoughts);
 
-router.get('/all', (req, res) => {
-    Thought.find({})
-        .sort({ createdAt: -1 }) //-1 - DESC 1 - ASC 
-        .then(dbThoughtData => {
-            res.json(dbThoughtData);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
+// create a thought for a user
+router
+  .route('/:userId')
+  .post(addThought)
 
+// Get, update, delete a thought by its id
+router.route('/:thoughtId')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(removeThought);
+
+// create a reaction to a thought
+router
+  .route('/:thoughtId/reactions')
+  .post(addReaction);
+
+// delecte a reaction by its id 
+router.route('/:thoughtId/reactions/:reactionId')
+  .delete(removeReaction);
 
 module.exports = router;
